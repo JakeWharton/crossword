50dc98b88571b38c94e37a7b07a3a50381767d6f
Jake Wharton <jw@squareup.com>
2022-02-22 21:55:38 -0500

    Kotlin 1.6.10, migrate to kotlin.test

--- src/test/java/com/jakewharton/crossword/TextCanvasTest.kt
+++ src/test/java/com/jakewharton/crossword/TextCanvasTest.kt
@@ -1,11 +1,11 @@
 package com.jakewharton.crossword
 
-import com.google.common.truth.Truth.assertThat
-import org.junit.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class TextCanvasTest {
   @Test fun zeroSized() {
     val output = TextCanvas(0, 0).toString()
-    assertThat(output).isEqualTo("")
+    assertEquals("", output)
   }
 }
